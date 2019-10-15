f15d67049047e273e9890bddddeda8e3cf079353
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-10-15 15:53:14 -0600

    finished mvp

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
+import Movie from "./Movie";
+import { NavLink } from "react-router-dom";
 
 const MovieCard = props => {
-  return;
+  const { title, director, metascore, stars } = props;
+  console.log(props);
+  return (
+    <NavLink to={`/movies/${props.id}`}>
+      <div className="movie-card">
+        <h2>{props.title}</h2>
+        <div className="movie-director">
+          Director: <em>{props.director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{props.metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars &&
+          stars.map(star => (
+            <div key={star} className="movie-star">
+              {star}
+            </div>
+          ))}
+      </div>
+    </NavLink>
+  );
 };
 
 export default MovieCard;
